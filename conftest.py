174cf22bc2b1fee53aeacf9ff4525302bba3ebb0
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-11-29 10:12:30 -0300

    bruh

--- conftest.py
+++ conftest.py
@@ -17,16 +17,16 @@ from pytest_metadata.plugin import metadata_key
 from Client.test_add_addres import (TEST_TITLE, QA, BACK, MOBILE)
 
 def pytest_html_report_title(report):  
-    report.title = f"{TEST_TITLE}"  
+    report.title = TEST_TITLE
   
 def pytest_configure(config):
     config.option.htmlpath = 'Reports/report.html'
   
     config.stash[metadata_key]["Projeto"] = "Bamburey"  
-    config.stash[metadata_key]["Qa"] = f"{QA}" 
+    config.stash[metadata_key]["QA"] = f"{QA}" 
     config.stash[metadata_key]["Back"] = f"{BACK}"
     config.stash[metadata_key]["Mobile"] = f"{MOBILE}" 
     
-    config.stash[metadata_key]["Hambiente"] = "Produção"
+    config.stash[metadata_key]["Ambiente"] = "Produção"
     config.stash[metadata_key]["Versão"] = "1.0.0"
     config.stash[metadata_key]["Build"] = "12345"
